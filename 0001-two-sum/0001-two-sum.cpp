380bec11caa68f4040246651062a4bafcbcf59c1
Gargi-Arora <aroragargi49@gmail.com>
2026-06-28 21:55:19 +0530

    Time: 0 ms (100%), Space: 14.9 MB (18.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
        map<int,int> mpp;
-       int n= nums.size();
-       for(int i=0;i<n;i++){
-        int num=nums[i];
-        int moreNeeded = target - num;
-        if(mpp.find(moreNeeded) != mpp.end()){
-            return{mpp[moreNeeded],i};
+       for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            int moreNeeded = target-num;
+            if(mpp.find(moreNeeded)!=mpp.end()){
+                return{mpp[moreNeeded],i};
+            }
+            mpp[num]=i;
         }
-        mpp[num]=i;
-       } 
-       return{-1,-1};
+        return{-1,-1};
     }
 };
