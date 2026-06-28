c6808e7596a0dd4a147bd16570a73c8f33f1696b
Gargi-Arora <aroragargi49@gmail.com>
2026-06-28 21:21:07 +0530

    Time: 0 ms (100%), Space: 20.7 MB (23.88%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,8 +3,8 @@ public:
     int singleNumber(vector<int>& nums) {
         int xorr=0;
         for(int i=0;i<nums.size();i++){
-            xorr = xorr ^ nums[i];
+            xorr= xorr^nums[i];
         }
-       return xorr; 
+        return xorr;
     }
 };
