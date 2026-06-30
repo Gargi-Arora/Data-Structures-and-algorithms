8fa6989f8a33b81be7eb75dfdb374a4fb2d9fadc
Gargi-Arora <aroragargi49@gmail.com>
2026-06-30 22:39:18 +0530

    Time: 2 ms (94.21%), Space: 26.2 MB (47.71%) - LeetHub

--- 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -11,17 +11,18 @@ public:
         if(nums.size()>threshold) return -1;
         int low=1, high = *max_element(nums.begin(),nums.end());
         int ans = high;
-        while(low<high){
+        while(low<=high){
             int mid = low + (high-low)/2;
             long long total = CheckDivisor(nums,mid);
             if(total<=threshold){
-                high = mid;
+                ans = mid;
+                high = mid-1;
             }
             else{
                 low=mid+1;
             }
 
         }
-        return high;
+        return ans;
     }
 };
