c160e13f202942a5038bac04bc91ff023c58cab0
Gargi-Arora <aroragargi49@gmail.com>
2026-07-25 16:09:28 +0530

    Time: 2 ms (15.37%), Space: 9.6 MB (63.83%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -4,17 +4,17 @@ public:
         long long ans = 1;
         vector<int> ansRow;
         ansRow.push_back(1);
-        for(int col=0;col<row;col++){
+        for(int col = 0;col<row;col++){
             ans = ans*(row-col);
             ans = ans/(col+1);
-            ansRow.push_back(ans);
+            ansRow.push_back(int(ans));
         }
         return ansRow;
     }
     vector<vector<int>> generate(int n){
         vector<vector<int>> ans;
-        for(int col = 0;col<n;col++){
-            ans.push_back(generateRows(col));
+        for(int i =0;i<n;i++){
+            ans.push_back(generateRows(i));
         }
         return ans;
     }
