b86d1e5dd12e8148fa453c5fee9b6b6f056bfd97
Gargi-Arora <aroragargi49@gmail.com>
2026-07-27 11:10:15 +0530

    Time: 20 ms (68.89%), Space: 17.6 MB (23.67%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -4,9 +4,10 @@ public:
         int n = nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-        for(int i=0;i<n;i++){
-            if(i>0&& nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n;j++){
+        for(int i = 0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            for(int j = i+1;j<n;j++){
                 if(j!=i+1 && nums[j]==nums[j-1]) continue;
                 int k = j+1;
                 int l = n-1;
@@ -15,19 +16,22 @@ public:
                     sum+= nums[j];
                     sum+= nums[k];
                     sum+= nums[l];
-                    if(sum==target){
+                    if(sum == target){
                         vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
                         ans.push_back(temp);
-                        k++;l--;
+                        k++,l--;
                         while(k<l && nums[k]==nums[k-1]) k++;
-                        while(k<l && nums[l]==nums[l+1])l--;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
                     else if(sum<target){
                         k++;
                     }
-                    else l--;
+                    else{
+                        l--;
+                    }
                 }
             }
+
         }
         return ans;
     }
