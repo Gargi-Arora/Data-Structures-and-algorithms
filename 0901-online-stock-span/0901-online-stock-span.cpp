d0c7dc2c058275f4478a42f482f6eb98999d544f
Gargi-Arora <aroragargi49@gmail.com>
2026-08-13 13:32:19 +0530

    Time: 40 ms (26.67%), Space: 92 MB (60.93%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,18 +1,20 @@
 class StockSpanner {
 public:
-    vector<int> arr;
+    stack<pair<int,int>> st;
+    int ind = -1;
     StockSpanner() {
+        ind = -1;
     }
     
     int next(int price) {
-        int cnt = 0;
-        arr.push_back(price);
-        cnt = 1;
-        for(int i = arr.size()-2;i>=0;i--){
-            if(arr[i]<=price) cnt++;
-            else break;
+        int ans;
+        ind++;
+        while(!st.empty() && st.top().first<=price){
+            st.pop();
         }
-        return cnt;
+        ans = ind - (st.empty()?-1:st.top().second);
+        st.push({price,ind});
+        return ans;
     }
 };
 
