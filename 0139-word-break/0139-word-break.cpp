030758ea50a46ce78e8c72fcee64bcf88e3b8613
Gargi-Arora <aroragargi49@gmail.com>
2026-09-04 16:52:39 +0530

    Time: 3 ms (71.32%), Space: 10.7 MB (83.17%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
-    bool wordBreakRec(int ind,string& s,vector<string> &wordDict, vector<int> &dp){
-       if(ind>= s.size()){
-         return true;
-       }
-       if(dp[ind]!= -1){
-         return dp[ind];
-       }
-       bool possible = false;
-       for(int i = 0;i<wordDict.size();i++){
-            string temp = wordDict[i];
-            if(temp.size()>s.size()-ind) continue;
-            bool ok = true;
-            int k = ind;
-            for(int j = 0;j<temp.size();j++){
-                if(temp[j]!=s[k]){
-                    ok = false;
-                    break;
-                }
-                else{
-                    k++;
-                }
-            }
-            if(ok){
-                possible |= wordBreakRec(ind + temp.size(),s,wordDict,dp);
-            }
-       }
-        return dp[ind] = possible;
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.size();
-        vector<int> dp(n+1,-1);
-        string temp = "";
-        return wordBreakRec(0,s,wordDict,dp);
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(string &w: wordDict){
+                int start = i-w.size();
+                if(start>=0 && dp[start] && s.substr(start,w.size())==w){
+                    dp[i]=1;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
