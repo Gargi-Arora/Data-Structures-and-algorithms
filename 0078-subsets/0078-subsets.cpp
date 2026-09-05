f5c4890bda3956da2ef39a5d765eedb2be00a4d5
Gargi-Arora <aroragargi49@gmail.com>
2026-09-05 15:34:50 +0530

    Time: 1 ms (42.21%), Space: 9.9 MB (59.52%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int subsets = 1<<nums.size();
         vector<vector<int>> ans;
-        for(int num = 0;num<=subsets-1;num++){
+        for(int n = 0;n<=subsets-1;n++){
             vector<int> list;
             for(int i = 0;i<=nums.size();i++){
-                if(num &(1<<i)) list.push_back(nums[i]);
+                if(n &(1<<i)) list.push_back(nums[i]);
             }
             ans.push_back(list);
         }
