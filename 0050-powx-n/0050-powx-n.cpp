60ab6f2077f846aa058388ca58f685861171fd3d
Gargi-Arora <aroragargi49@gmail.com>
2026-09-08 14:14:17 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.89%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        long long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+    double power(double x,long n){
+        if(n==0) return 1.0;
+        if(n==1) return x;
+        if(n%2==0){
+            return power(x*x,n/2);
         }
-        return fastPow(x, N);
+        return x*power(x,n-1);
     }
-    
-private:
-    double fastPow(double x, long long n) {
-        if (n == 0) return 1.0;
-        double half = fastPow(x, n / 2);
-        double result = half * half;
-        if (n % 2 == 1) result *= x;
-        return result;
+    double myPow(double x, int n) {
+        long num = n;
+        if(num<0){
+            return (1.0/power(x,(-1*num)));
+        }
+        return power(x,num);
     }
 };
